fa86cfb2991d40f9f4af14595b49317fc06bb54b
happyjamy <juwhan1004@naver.com>
2022-05-19 22:20:01 +0900

    comlete unit,e2e testing

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -12,7 +13,101 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe("getAll", ()=>{
+    it("should return an array", () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    } )
+  })
+
+  describe("getOne", ()=>{
+    it("should return a movie", ()=>{
+      service.create({
+        title: "test",
+        year: 2020,
+        genres: ["horror"]
+      })
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    })
+
+    it("should throw a NotFoundException", ()=>{
+      try{
+        service.getOne(123232);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual("Movie with ID 123232 not found");
+      }
+    })
+  })
+
+  describe("delete", ()=>{
+    it("should delete a movie", ()=>{
+      service.create({
+        title: "test",
+        year: 2020,
+        genres: ["horror"]
+      })
+      const beforeMovies = service.getAll();
+      service.delete(1);
+      const afterMovies = service.getAll();
+      expect(afterMovies.length).toBeLessThan(beforeMovies.length);
+    })
+    
+    it("should throw a NotFoundException", ()=>{
+      try{
+        service.delete(123232);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual("Movie with ID 123232 not found");
+      }
+    })
+  })
+
+  describe("update", ()=>{
+    it("should update a movie", ()=>{
+      service.create({
+        title: "test",
+        year: 2020,
+        genres: ["horror"]
+      })
+      service.update(1,{title: "test2"})
+      const afterMovies = service.getOne(1);
+
+      expect(afterMovies.title).toEqual("test2");
+    })
+
+    it("should throw a NotFoundException", ()=>{
+      try{
+        service.update(123232,{title: "test2"});
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual("Movie with ID 123232 not found");
+      }
+    })
+  })
 });
+
+/*
+beforeEach(fn, timeout)
+각각의 테스트가 실행되기 전에 매번 함수를 실행합니다.
+각각의 테스트 전에 각 테스트에서 사용할 전역 상태를 재설정하려는 경우에 유용합니다.
+함수가 promise을 반환하거나 generator인 경우 Jest는 테스트를 실행하기 전에 해당 promise가 해결될 때까지 기다립니다.
+밀리초로 대기할 시간을 지정할 수 있습니다. (기본 시간 5초)
+https://jestjs.io/docs/api#beforeeachfn-timeout
+
+beforeAll(fn, timeout)
+모든 테스트가 실행되기 전에 딱 한 번 함수를 실행합니다.
+
+afterEach(fn, timeout)
+각각의 테스트가 완료된 후 함수를 실행합니다.
+
+afterAll(fn, timeout)
+모든 테스트가 완료된 후 함수를 실행합니다.
+*/
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true, // 아무 데코레이터도 없는 속성의 object는 거름.
+      forbidNonWhitelisted: true, // 리퀘스트 자체를 막음. 에러 send
+      transform: true, // 유저가 보낸 data 타입을 실제 원하는 타입으로 변환해줌
+    }));
     await app.init();
   });
 
@@ -19,6 +24,72 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to jamy homepage');
   });
+
+  describe('/movies', ()=>{
+    it('GET', ()=>{
+      return request(app.getHttpServer())
+      .get('/movies')
+      .expect(200)
+      .expect([]);
+    })
+
+    it('POST 201', ()=>{
+      return request(app.getHttpServer())
+      .post('/movies')
+      .send({
+        title: "test",
+        year: 2020,
+        genres: ["horror"]
+      })
+      .expect(201)
+    })
+
+    it('POST 400', ()=>{
+      return request(app.getHttpServer())
+      .post('/movies')
+      .send({
+        title: "test",
+        genres: ["horror"]
+      })
+      .expect(400)
+    })
+
+    it('DELETE', ()=>{
+      return request(app.getHttpServer())
+      .delete('/movies')
+      .expect(404);
+    })
+
+    describe('/movies/:id', ()=>{
+      it('GET 200', ()=>{
+        return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200)
+      })
+      it('GET 404', ()=>{
+        return request(app.getHttpServer())
+        .get('/movies/123232')
+        .expect(404)
+      })
+
+      it('PATCH', ()=>{
+        return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({
+          title: "test2",
+          year: 2020,
+          genres: ["horror"]
+        })
+        .expect(200)
+      })
+
+      it('DELETE', ()=>{
+        return request(app.getHttpServer())
+        .delete('/movies/1')
+        .expect(200);
+      })
+    })
+  })
 });
